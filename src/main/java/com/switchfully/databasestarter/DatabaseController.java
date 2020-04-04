d52fab8f563e12d09344c29b28290233f83af7e9
karelVR <karel.vanroey@cegeka.com>
2020-04-04 19:17:59 +0200

    Start using ScriptUtils

--- src/main/java/com/switchfully/databasestarter/DatabaseController.java
+++ src/main/java/com/switchfully/databasestarter/DatabaseController.java
@@ -2,21 +2,32 @@ package com.switchfully.databasestarter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @RestController
 @RequestMapping(path = "/database")
 public class DatabaseController {
-    ApplicationContext context;
+    final ApplicationContext context;
+
+    final DataSource dataSource;
 
     @Autowired
-    public DatabaseController(ApplicationContext context) {
+    public DatabaseController(ApplicationContext context, DataSource dataSource) {
         this.context = context;
+        this.dataSource = dataSource;
     }
 
-    @GetMapping public String[] getInfo(){
-        return context.getBeanDefinitionNames();
+    @GetMapping
+    public String createSimpleTable() throws SQLException {
+
+        ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql/hello.sql"));
+        return "OK";
     }
 }
